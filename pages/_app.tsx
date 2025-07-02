b24f3c0b250814dc65c2ab999cb49c46ed7f8624
caio.imperial <scaioimperial@gmail.com>
2025-07-01 23:48:15 -0300

    refactor: Wrap main application in Layout component for improved structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/css/style.css";
 import Head from "next/head";
 import { AppProps } from 'next/app';
+import Layout from "@/components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
