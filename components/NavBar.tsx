9c15cc76414750e123072d7d05f95e650ca471f8
caio.imperial <scaioimperial@gmail.com>
2025-07-01 23:48:02 -0300

    feat: Add NavBar component for navigation in the Bonsai App

---
+++ components/NavBar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">🌱 Bonsai App</a>
+      </div>
+    </nav>
+  );
+}
