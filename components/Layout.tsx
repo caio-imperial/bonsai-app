be73f8970babb27d5eb171d9f34bb5ab2dfcdf8c
caio.imperial <scaioimperial@gmail.com>
2025-07-03 13:36:57 -0300

    refactor: stick sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,12 +3,15 @@ import Sidebar from './Sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <Sidebar>
-                <main className="container py-4">
-                    {children}
-                </main>
-            </Sidebar>
-        </>
+        <div className="container-fluid">
+            <div className="row vh-100">
+                <div className="col-auto col-md-3 col-xl-2 px-0">
+                    <Sidebar />
+                </div>
+            <div className="col vh-100 overflow-auto px-4 py-3">
+                {children}
+            </div>
+            </div>
+        </div>
     )
 }
