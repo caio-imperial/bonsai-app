179d913baef22cf59c0a0b7e99c49e83a0c4427b
caio.imperial <scaioimperial@gmail.com>
2025-07-06 19:52:20 -0300

    refactor: Replace local state management with custom hooks for bonsai and entry data retrieval

--- pages/bonsais/[bonsaiId]/index.tsx
+++ pages/bonsais/[bonsaiId]/index.tsx
@@ -1,40 +1,30 @@
 // pages/bonsai/[id].tsx
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Timeline from "@/components/Timeline";
-import { Bonsai, Entry } from "@/types";
 import { Button } from "@/components/ui/button";
 import { TypographyH3, TypographyMuted, TypographyH1 } from "@/components/ui/typography";
 import { ArrowLeftIcon, PencilIcon, PlusIcon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useEntry } from "@/hooks/useEntry";
+import { useDeleteEntry } from "@/hooks/useDeleteEntry";
+import { toast } from "sonner";
+import { useBonsai } from "@/hooks/useBonsai";
 
 export default function TimelinePage() {
   const router = useRouter();
   const { bonsaiId } = router.query;
 
-  const [bonsai, setBonsai] = useState<Bonsai | null>(null);
-  const [entries, setEntries] = useState<Entry[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (!bonsaiId) return;
-
-    fetch(`/api/bonsais/${bonsaiId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setBonsai(data.bonsai);
-        setEntries(data.entries);
-        setLoading(false);
-      });
-  }, [bonsaiId]);
+  const { entries, loading: entriesLoading, setEntries } = useEntry(bonsaiId as string | undefined);
+  const { bonsai, loading: bonsaiLoading } = useBonsai(bonsaiId as string | undefined);
+  const { deleteEntry, error } = useDeleteEntry();
 
   const handleDelete = (id: string) => {
-    fetch(`/api/bonsais/${bonsaiId}/entries/${id}`, {
-      method: "DELETE",
-    }).then(() => {
-        setEntries(entries.filter((entry) => entry._id !== id));
-      });
+    if (!entries) return;
+    deleteEntry(bonsaiId as string, id);
+    if (error) toast.error(error);
+    else toast.success("Registro deletado com sucesso!");
+    setEntries(entries?.filter((entry) => entry?._id !== id) || []);
   };
 
   return (
@@ -44,7 +34,7 @@ export default function TimelinePage() {
       </Link>
       <div className="flex justify-between gap-2">
         <div className="w-full">
-          {loading ? (
+          {bonsaiLoading ? (
             <>
               <Skeleton className="w-full h-4 mb-1" />
               <Skeleton className="w-full h-8" />
@@ -61,27 +51,27 @@ export default function TimelinePage() {
           )}
         </div>
         <div className="flex gap-2">
-          <Button variant="secondary" disabled={loading}>
+          <Button variant="secondary" disabled={entriesLoading}>
             <Link href={`/bonsais/${bonsaiId}/edit`} className="flex items-center gap-2">
               <PencilIcon className="w-4 h-4" /> Editar
             </Link>
           </Button>
-          <Button variant="secondary" disabled={loading}>
+          <Button variant="secondary" disabled={entriesLoading}>
             <Link href={`/bonsais/${bonsaiId}/entries`}>
               <PlusIcon className="w-4 h-4" />
             </Link>
           </Button>
         </div>
       </div>
-      {loading ? (
+      {entriesLoading ? (
         <>
           <Skeleton className="w-full h-96" />
         </>
-      ) : entries.length === 0 ? (
+      ) :  entries && entries.length === 0 ? (
         <TypographyH1>Nenhum registro ainda 😢</TypographyH1>
       ) : (
         <Timeline
-          entries={entries}
+          entries={entries || []}
           handleDelete={handleDelete}
         />
       )}
