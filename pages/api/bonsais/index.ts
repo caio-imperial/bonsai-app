99d927e0d9155fae09bca770d227ccf5beb1e085
caio.imperial <scaioimperial@gmail.com>
2025-07-05 22:33:27 -0300

    feat: Add validation for bonsai creation to ensure 'name' attribute is provided

--- pages/api/bonsais/index.ts
+++ pages/api/bonsais/index.ts
@@ -5,6 +5,12 @@ import { createBonsai, getBonsais } from '@/lib/data'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { name, species } = req.body
+    if (!name) {
+      return res.status(422).json({ 
+        error: "Atributo name e obrigatório",
+        message: "Para registrar um bonsai, é necessário informar o nome",
+      });
+    }
     const id = await createBonsai({ name, species })
     res.status(200).json({ id })
   } else if (req.method === 'GET') {
