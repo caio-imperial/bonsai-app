ef95c3c991dfa2ddfaad34da4f2f5a4570189f71
caio.imperial <scaioimperial@gmail.com>
2025-07-05 23:00:29 -0300

    feat: Enhance bonsai listing page with search functionality and loading state, integrating new UI components for improved user experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 // pages/index.tsx
 import { useEffect, useState } from "react";
+import { TypographyH1 } from "@/components/ui/typography";
+import { CardBonsai } from "@/components/Card/Bonsai";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 import Link from "next/link";
-import { useConfirm } from "@/context/ConfirmContext";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Bonsai = {
   _id: string;
@@ -11,76 +16,72 @@ type Bonsai = {
 
 export default function Home() {
   const [bonsais, setBonsais] = useState<Bonsai[]>([]);
-  const { showConfirm } = useConfirm();
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch("/api/bonsais")
       .then((res) => res.json())
-      .then((data) => setBonsais(data));
+      .then((data) => {
+        setBonsais(data);
+        setLoading(false);
+      });
   }, []);
 
-  const handleDelete = async (
-    e: React.MouseEvent,
-    id: string,
-    bonsaiName: string
-  ) => {
-    e.preventDefault();
-    showConfirm({
-      title: "Excluir bonsai?",
-      message: `Você tem certeza que deseja excluir "${bonsaiName}"?`,
-      confirmText: "Sim, excluir",
-      cancelText: "Cancelar",
-      onConfirm: () => {
-        fetch(`/api/bonsais/${id}`, {
-          method: "DELETE",
-        });
-        setBonsais(bonsais.filter((bonsai) => bonsai._id !== id));
-      },
-    });
-  };
+  const matchingBonsais = bonsais.filter((bonsai) =>
+    bonsai.name && bonsai.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div className="container">
-      <div className="d-grid gap-2 d-md-flex justify-content-md-between align-items-md-center mb-2 mb-md-4">
-        <h1 className="mb-0">🌱 Meus Bonsais</h1>
-        <Link href="/bonsais/new" className="btn btn-primary btn-sm">
-          <i className="bi bi-plus" /> Novo Bonsai
-        </Link>
+    <main className="w-full flex flex-col gap-4 mt-8">
+      <div className="mb-4">
+        <TypographyH1>🌱 Meus Bonsais</TypographyH1>
       </div>
 
-      {bonsais.length === 0 ? (
-        <p>Você ainda não tem nenhum bonsai cadastrado 😢</p>
-      ) : (
-        <ul className="list-group">
-          {bonsais.map((bonsai) => (
-            <li
-              key={bonsai._id}
-              className="d-flex justify-content-between align-items-center list-group-item "
-            >
-              <Link href={`/bonsais/${bonsai._id}`} className="link-dark">
-                <strong>{bonsai.name}</strong>
-                {bonsai.species && (
-                  <span className="text-muted"> — {bonsai.species}</span>
-                )}
+      <div className="flex gap-2">
+        <Input
+          type="text"
+          placeholder="Nome do bonsai"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          disabled={loading}
+        />
+        <Button
+          className="cursor-pointer hover:bg-primary/90"
+          variant="default"
+          size="icon"
+          disabled={loading}
+        >
+          <Link href="/bonsais/new">
+            <PlusIcon className="w-4 h-4" />
+          </Link>
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 animate-in fade-in-0 duration-300">
+        {loading ? (
+          <>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <Skeleton key={index} className="w-full h-27" />
+            ))}
+          </>
+        ) : (
+          bonsais.length === 0 ? (
+            <p className="text-center text-sm text-muted-foreground">Você ainda não tem nenhum bonsai cadastrado 😢</p>
+        ) : (
+          <>
+            {matchingBonsais.map((bonsai) => (
+              <Link
+                href={`/bonsais/${bonsai._id}`}
+                key={bonsai._id}
+              >
+                <CardBonsai name={bonsai.name} species={bonsai.species} />
               </Link>
-              <div className="btn-group me-2">
-                <Link
-                  href={`/bonsais/${bonsai._id}/edit`}
-                  className="btn btn-primary btn-sm"
-                >
-                  <i className="bi bi-pencil" />
-                </Link>
-                <button
-                  onClick={(e) => handleDelete(e, bonsai._id, bonsai.name)}
-                  className="btn btn-danger btn-sm"
-                >
-                  <i className="bi bi-trash" />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+            ))}
+          </>
+        ))}
+      </div>
+    </main>
   );
 }
