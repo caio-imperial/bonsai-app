cc0282d05538e830ab535128376aa53fdeea7e3b
caio.imperial <scaioimperial@gmail.com>
2025-07-07 11:49:27 -0300

    feat: Add normalizeString utility function to standardize string formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const normalizeString = (str: string) =>
+  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
